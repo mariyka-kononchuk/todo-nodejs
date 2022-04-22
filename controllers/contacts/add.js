1edfa9446a309b754e8c1056dc29a15ac4559100
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2022-04-22 14:24:27 +0300

    date format fixed

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,12 +1,15 @@
 const { Contact } = require('../../models');
-const dateFormat = require('dateformat');
+// const dateFormat = require('dateformat');
 // import dateFormat from "dateformat";
+const moment = require('moment');
 const { getDatesInContent } = require('../../helpers');
 
 const add = async (req, res) => {
   const { name, category, content } = req.body;
   const dates = getDatesInContent(content);
-  const date = dateFormat(new Date(), "mmmm d, yyyy");
+  // const date = dateFormat(new Date(), "mmmm d, yyyy");
+  const date = moment().format('MMMM D, YYYY')
+  // const date = new Date();
   const note = {
     name,
     date,
