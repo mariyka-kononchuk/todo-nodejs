fa137f8608e9fc856bba2afa96aee4a25b9ec577
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2022-04-22 14:31:38 +0300

    field dates fixed

--- helpers/getDatesInContent.js
+++ helpers/getDatesInContent.js
@@ -1,23 +1,16 @@
-// const sgMail = require('@sendgrid/mail');
-// const dotenv = require('dotenv');
+const moment = require('moment');
 
 function getDatesInContent (content) {
     const datesFound = content.match(/\d{1,2}\D\d{1,2}\D(\d{4}|\d{2})/g);
     let contentDates= [];
     let dates = ''
     if (datesFound !== null) {
-        datesFound.map(item => contentDates.push(dateFormat(item, "m/d/yyyy")))
+        datesFound.map(item => contentDates.push(moment(item).format("MM/DD/YYYY")))
         dates = contentDates.join(', ');
+        return dates;
     }
     return
 }
-
-// const email = {
-//   to: 'mariyka.konochuk@gmail.com',
-//   from: 'koomedna@gmail.com',
-//   subject: 'New application from site',
-//   html:'<p>Hello</p>'
-// }
 
 module.exports = getDatesInContent;
 
